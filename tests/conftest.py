4163734c4f0d4a73be27df3803bc358ecfc2d6a9
Sasa <savilasousa87@gmail.com>
2026-06-01 04:01:48 -0300

    fix: corrige imports dos testes no GitHub Actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 import pytest  # type: ignore
 
 from repositories.repositorio_emprestimo import RepositorioEmprestimo
