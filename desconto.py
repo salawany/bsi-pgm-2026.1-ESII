ebfde5070a69baaf3aa749751ae384e64f8c3fad
Sasa <savilasousa87@gmail.com>
2026-06-09 20:45:30 -0300

    refactor: extrai calculo de dias adiantados (ciclo 3)

--- desconto.py
+++ desconto.py
@@ -3,5 +3,16 @@ def calcular_desconto_devolucao_antecipada(
     data_devolucao,
     valor_por_dia
 ):
-    dias_adiantados = (data_prevista - data_devolucao).days
+    dias_adiantados = _calcular_dias_adiantados(
+        data_prevista,
+        data_devolucao
+    )
+
     return dias_adiantados * valor_por_dia
+
+
+def _calcular_dias_adiantados(
+    data_prevista,
+    data_devolucao
+):
+    return (data_prevista - data_devolucao).days
